d1f02568c4f1bdaa2ea8b9b65adf3081d976edd1
pipe <prashant.ipe@krds.com>
2019-10-19 16:29:30 +0530

    adding a visibility check

---
+++ js/visibility.js
@@ -0,0 +1,36 @@
+var hidden, state, visibilityChange; 
+if (typeof document.hidden !== "undefined") {
+	hidden = "hidden";
+	visibilityChange = "visibilitychange";
+	state = "visibilityState";
+} else if (typeof document.mozHidden !== "undefined") {
+	hidden = "mozHidden";
+	visibilityChange = "mozvisibilitychange";
+	state = "mozVisibilityState";
+} else if (typeof document.msHidden !== "undefined") {
+	hidden = "msHidden";
+	visibilityChange = "msvisibilitychange";
+	state = "msVisibilityState";
+} else if (typeof document.webkitHidden !== "undefined") {
+	hidden = "webkitHidden";
+	visibilityChange = "webkitvisibilitychange";
+	state = "webkitVisibilityState";
+}
+
+var visBar = document.querySelector("#visibilityBar");
+// Add a listener that constantly changes the title
+document.addEventListener(visibilityChange, function() {
+	var today = new Date();
+//var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+//var dateTime = date+' '+time;
+    if(document[state] == "hidden"){
+		stateMsg = "Hey!, Dont Try going away!! Resetting Timer."
+		clearTimer();
+	}
+    
+	visibilityBar.innerHTML += stateMsg+" @ "+time+"<br>";
+}, false);
+
+// Set the initial value
+//visibilityBar.innerHTML = document[state]+"<br>";
